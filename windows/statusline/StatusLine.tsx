ada5f12060d7482ceae91c0bbd3ee76fa4eca513
TanX-009 <tanmaymuley009@gmail.com>
2025-01-16 11:32:11 +0530

    feat(statusline): add volume and mic

--- windows/statusline/StatusLine.tsx
+++ windows/statusline/StatusLine.tsx
@@ -1,88 +1,100 @@
-import Hyprland from 'gi://AstalHyprland'
-import Battery from 'gi://AstalBattery'
+import Hyprland from "gi://AstalHyprland";
+import Battery from "gi://AstalBattery";
+import Wp from "gi://AstalWp";
 
-import { App, Astal, Gdk, Gtk } from 'astal/gtk3'
-import { bind, Variable } from 'astal'
+import { App, Astal, Gdk, Gtk } from "astal/gtk3";
+import { bind, Variable } from "astal";
 
-import BatteryIcon from '@widgets/BatteryIcon'
-import StatusLineDivider from '@widgets/StatusLineDivider'
+import BatteryIcon from "@widgets/BatteryIcon";
+import StatusLineDivider from "@widgets/StatusLineDivider";
 
-import NormalMode from './modes/NormalMode'
-import CommandMode from './modes/CommandMode'
-import WallpapersMode from './modes/WallpapersMode'
+import NormalMode from "./modes/NormalMode";
+import CommandMode from "./modes/CommandMode";
+import WallpapersMode from "./modes/WallpapersMode";
 
-import { statusLineMode } from './vars'
-import AppLauncherMode from './modes/AppLauncherMode'
+import { statusLineMode } from "./vars";
+import AppLauncherMode from "./modes/AppLauncherMode";
 
-const hyprland = Hyprland.get_default()
-const battery = Battery.get_default()
+const hyprland = Hyprland.get_default();
+const battery = Battery.get_default();
+const speaker = Wp.get_default()?.audio.defaultSpeaker!;
+const microphone = Wp.get_default()?.audio.defaultMicrophone!;
 
-const time = Variable('')
-  .poll(1000, `date "+%I : %M %p"`)
-
+const time = Variable("").poll(1000, `date "+%I : %M %p"`);
 
 function RightSection() {
   return (
-    <box className='right'>
-      <label
-        className='decoration'
-        label=''
-      />
+    <box className="right">
+      <label className="decoration" label="󰣇" />
 
       <StatusLineDivider />
 
-      <box visible={bind(battery, 'isPresent')}>
-        <box
-          className='battery'
-          halign={Gtk.Align.CENTER}
-          spacing={8}>
-          <BatteryIcon className='icon' />
+      <box visible={bind(battery, "isPresent")}>
+        <box className="battery" halign={Gtk.Align.CENTER} spacing={8}>
+          <icon icon={bind(battery, "batteryIconName")} />
 
           <label
-            className='percentage'
-            label={
-              bind(battery, 'percentage')
-              .as(percentage => `${Math.floor(percentage * 100)}%`)
-            }
+            className="percentage"
+            label={bind(battery, "percentage").as(
+              (percentage) => `${Math.floor(percentage * 100)}%`,
+            )}
           />
         </box>
 
         <StatusLineDivider />
       </box>
 
-      <label
-        className='user'
-        label={USER}
-      />
+      <box className="volume" halign={Gtk.Align.CENTER} spacing={8}>
+        <icon icon={bind(speaker, "volumeIcon")} />
 
-      <label
-        className='time_indicator'
-        label={time()}
-      />
+        <label
+          className="percentage"
+          label={bind(speaker, "volume").as(
+            (percentage) => `${Math.floor(percentage * 100)}%`,
+          )}
+        />
+      </box>
+      <StatusLineDivider />
+
+      <box className="mic" halign={Gtk.Align.CENTER} spacing={8}>
+        <icon icon={bind(microphone, "volumeIcon")} />
+
+        <label
+          className="percentage"
+          label={bind(microphone, "volume").as(
+            (percentage) => `${Math.floor(percentage * 100)}%`,
+          )}
+        />
+      </box>
+      <StatusLineDivider />
+
+      <label className="user" label={USER} />
+
+      <label className="time_indicator" label={time()} />
 
       <stack
-        className='mode_indicator'
-        shown='workspace'
+        className="mode_indicator"
+        shown="workspace"
         transitionType={Gtk.StackTransitionType.OVER_RIGHT_LEFT}
-        transitionDuration={ANIMATION_SPEED}>
+        transitionDuration={ANIMATION_SPEED}
+      >
         <label
-          name='workspace'
-          className='workspace'
-          label={
-            bind(hyprland, 'focusedWorkspace')
-              .as(workspace => `${workspace.get_id()}:0`)
-          }
+          name="workspace"
+          className="workspace"
+          label={bind(hyprland, "focusedWorkspace").as(
+            (workspace) => `${workspace.get_id()}:0`,
+          )}
         />
       </stack>
     </box>
-  )
+  );
 }
 
 function StatusLine(props: { gdkmonitor: Gdk.Monitor }) {
-  const { gdkmonitor } = props
+  const { gdkmonitor } = props;
 
   return (
-    <box className='statusline'>
+    <box className="statusline">
       <stack shown={statusLineMode()}>
         <NormalMode />
         <AppLauncherMode gdkmonitor={gdkmonitor} />
@@ -92,25 +104,28 @@ function StatusLine(props: { gdkmonitor: Gdk.Monitor }) {
 
       <RightSection />
     </box>
-  )
+  );
 }
 
-export default function(gdkmonitor: Gdk.Monitor) {
+export default function (gdkmonitor: Gdk.Monitor) {
   return (
     <window
-      name='statusline'
-      namespace='astal_statusline'
+      name="statusline"
+      namespace="astal_statusline"
       application={App}
       gdkmonitor={gdkmonitor}
       exclusivity={Astal.Exclusivity.EXCLUSIVE}
       layer={Astal.Layer.TOP}
-      anchor={Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT | Astal.WindowAnchor.BOTTOM}
-      keymode={statusLineMode(mode =>
-        mode === 'normal'
-          ? Astal.Keymode.NONE
-          : Astal.Keymode.EXCLUSIVE
-      )}>
+      anchor={
+        Astal.WindowAnchor.LEFT |
+        Astal.WindowAnchor.RIGHT |
+        Astal.WindowAnchor.BOTTOM
+      }
+      keymode={statusLineMode((mode) =>
+        mode === "normal" ? Astal.Keymode.NONE : Astal.Keymode.EXCLUSIVE,
+      )}
+    >
       <StatusLine gdkmonitor={gdkmonitor} />
     </window>
-  )
+  );
 }
--- windows/statusline/modes/NormalMode.tsx
+++ windows/statusline/modes/NormalMode.tsx
@@ -1,92 +1,90 @@
-import Hyprland from 'gi://AstalHyprland'
-import Mpris from 'gi://AstalMpris'
+import Hyprland from "gi://AstalHyprland";
+import Mpris from "gi://AstalMpris";
 
-import StatusLineDivider from '@widgets/StatusLineDivider'
+import StatusLineDivider from "@widgets/StatusLineDivider";
 
-import { Gtk } from 'astal/gtk3'
-import { bind } from 'astal'
+import { Gtk } from "astal/gtk3";
+import { bind } from "astal";
+import {
+  EventBox,
+  Scrollable,
+} from "../../../../../../../../usr/share/astal/gjs/gtk3/widget";
 
-const hyprland = Hyprland.get_default()
-const spotify = Mpris.Player.new('spotify')
+const hyprland = Hyprland.get_default();
+const spotify = Mpris.Player.new("brave.instance36292");
+const player = Mpris.get_default();
 
 export default function NormalMode() {
   return (
-    <box
-      name='normal'
-      className='normal'
-      spacing={8}
-      hexpand={true}>
+    <box name="normal" className="normal" spacing={8} hexpand={true}>
       <stack
-        className='window_state'
+        className="window_state"
         transitionType={Gtk.StackTransitionType.OVER_RIGHT_LEFT}
         transitionDuration={ANIMATION_SPEED}
         setup={(self) => {
-          self.hook(hyprland, 'event', () => {
-            const focusedClient = hyprland.get_focused_client()
+          self.hook(hyprland, "event", () => {
+            const focusedClient = hyprland.get_focused_client();
             if (!focusedClient) {
-              self.set_shown('tiling')
-              return
+              self.set_shown("tiling");
+              return;
             }
 
             self.set_shown(
-              !focusedClient.get_floating()
-                ? 'tiling' : 'floating'
-            )
-          })
-        }}>
-        <label
-          name='tiling'
-          className='tiling'
-          label='TILING'
-        />
+              !focusedClient.get_floating() ? "tiling" : "floating",
+            );
+          });
+        }}
+      >
+        <label name="tiling" className="tiling" label="TILING" />
 
-        <label
-          name='floating'
-          className='floating'
-          label='FLOATING'
-        />
+        <label name="floating" className="floating" label="FLOATING" />
       </stack>
 
       <label
-        className='active_window'
+        className="decoration"
         setup={(self) => {
-          self.hook(hyprland, 'event', () => {
-            const focusedClient = hyprland.get_focused_client()
+          self.hook(hyprland, "event", () => {
+            const focusedClient = hyprland.get_focused_client();
 
-            self.set_label(
-              focusedClient
-                ? focusedClient.get_class()
-                : '~'
-            )
-          })
+            self.set_label(focusedClient ? focusedClient.get_title() : "~");
+            //self.set_label(
+            //  focusedClient ? focusedClient.get_title() + " -" : "~ -",
+            //);
+          });
         }}
       />
 
-      <label
-        className='decoration'
-        label='[+]'
-      />
+      {/*
+        <label
+          className="active_window"
+          setup={(self) => {
+            self.hook(hyprland, "event", () => {
+              const focusedClient = hyprland.get_focused_client();
+
+              self.set_label(focusedClient ? focusedClient.get_class() : "~");
+            });
+          }}
+        />
+      */}
 
       <StatusLineDivider />
 
-      {bind(spotify, 'available').as(musicAvailable =>
-        !musicAvailable
-          ? (
-              <label
-                className='music_indicator'
-                label='󰝛 No Music - Title'
-              />
-            )
-          : (
-              <label
-                className='music_indicator'
-                label={
-                  bind(spotify, 'title')
-                    .as(title => `󰝚 Playing - ${title}`)
-                }
-              />
-            )
-      )}
+      <EventBox
+        onScroll={(self, event) => {
+          console.log(event.modifier);
+        }}
+      >
+        {bind(spotify, "available").as((musicAvailable) =>
+          !musicAvailable ? (
+            <label className="music_indicator" label="󰝛 No Music - Title" />
+          ) : (
+            <label
+              className="music_indicator"
+              label={bind(spotify, "title").as((title) => `󰝚 ${title}`)}
+            />
+          ),
+        )}
+      </EventBox>
     </box>
-  )
+  );
 }
